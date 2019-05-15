bd89a4aa4f6b2d58d5bf46f1bda40f9187c54ab6
ScottCWatman <48034730+ScottCWatman@users.noreply.github.com>
2019-05-15 11:42:43 -0400

    PlayerController(Version 1)
    Basic Player Movement

---
+++ KajiuCollesuem/Assets/Scripts/PlayerController_V1.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController_V1 : MonoBehaviour
+{
+    public float speed = 1.0f;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        float translation = Input.GetAxis("Vertical");
+        float straffe = Input.GetAxis("Horizontal");
+        Vector3 move = new Vector3(straffe, 0, translation).normalized * Time.deltaTime * speed;
+
+        transform.Translate(move);
+    }
+}
