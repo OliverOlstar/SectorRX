ce45016df1be9ce59cef868a0a47c937d65df5fa
Mugiesshan Anandarajah <manandarajah@users.noreply.github.com>
2019-05-15 12:29:55 -0400

    Enemy Collision

---
+++ KajiuCollesuem/Assets/Scripts/EnemyCollision.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyCollision : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Slider slide = null;
+
+        if (collision.gameObject.name.Contains("Dummy"))
+        {
+            slide = GameObject.Find("AISlider").GetComponent<Slider>();
+        }
+
+        else if (collision.gameObject.name.Contains("Player"))
+        {
+            slide = GameObject.Find("PlayerSlider").GetComponent<Slider>();
+        }
+        
+        if (slide != null)
+        {
+            slide.value -= 10;
+        }
+    }
+}
