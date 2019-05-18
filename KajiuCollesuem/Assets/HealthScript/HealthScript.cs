d514e81766da1b05f0159be5165ad4aaf9fdce1b
kavianthecraazy <48034454+kavianthecraazy@users.noreply.github.com>
2019-05-18 17:52:49 -0400

    Kavian Health Script
    Most recent health script created by Kavian ;)

---
+++ KajiuCollesuem/Assets/HealthScript/HealthScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthScript : MonoBehaviour
+{
+    public int startingHealth;
+    public int currentHealth;
+    public Slider healthSlider;
+    private bool isDead;
+
+    void Start()
+    {
+        currentHealth = startingHealth;
+    }
+
+    void TakeDamage(int damageAmount)
+    {
+        currentHealth -= damageAmount;
+        healthSlider.value = currentHealth;
+        if (currentHealth <= 0 && !isDead)
+        {
+            isDead = true;
+        }
+    }
+}
