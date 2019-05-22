47eb4477428fb816b462b8d61df1301043f5731c
Oliver <Olstar_123@users.noreply.github.com>
2019-05-21 21:13:32 -0400

    Adding Robs Script (PlayerVars)

--- KajiuCollesuem/Assets/Scripts/EnemyCollision.cs
+++ KajiuCollesuem/Assets/Scripts/EnemyCollision.cs
@@ -5,11 +5,12 @@ using UnityEngine.UI;
 
 public class EnemyCollision : MonoBehaviour
 {
+    private PlayerAttributes playerVars;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        playerVars = GetComponent<PlayerAttributes>();
     }
 
     // Update is called once per frame
@@ -20,21 +21,24 @@ public class EnemyCollision : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        Slider slide = null;
+        //Slider slide = null;
 
-        if (collision.gameObject.name.Contains("Dummy"))
+        if (collision.gameObject.tag == "Enemy")
         {
-            slide = GameObject.Find("AISlider").GetComponent<Slider>();
+            //slide = GameObject.Find("PlayerSlider").GetComponent<Slider>();
+            playerVars.takeDamage(10);
         }
 
+        /*
         else if (collision.gameObject.name.Contains("Player"))
         {
-            slide = GameObject.Find("PlayerSlider").GetComponent<Slider>();
+            slide = GameObject.Find("AISlider").GetComponent<Slider>();
         }
         
         if (slide != null)
         {
             slide.value -= 10;
         }
+        */
     }
 }
---
+++ KajiuCollesuem/Assets/Scripts/PlayerAttributes.cs
@@ -0,0 +1,131 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerAttributes : MonoBehaviour
+{
+    private readonly int maxHealth = 100;
+    private int health;
+
+    private readonly int maxShield = 100;
+    private int shield;
+
+    private readonly int maxPowerGuage = 10;
+    private int powerGuage;
+
+    public Slider healthSlider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = maxHealth;
+        shield = maxShield;
+        powerGuage = 0;
+    }
+
+    //GET SET
+    //get current variables
+    public int getHealth()
+    {
+        return health;
+    }
+
+    public int getShield()
+    {
+        return shield;
+    }
+
+    public int getPowerGuage()
+    {
+        return powerGuage;
+    }
+
+    //METHODS
+    //gain health
+    public void gainHealth(int x)
+    {
+        //make sure can't gain more health than max
+        if (health + x >= maxHealth)
+        {
+            health = maxHealth;
+        }
+        else
+        {
+            health += x;
+            healthSlider.value = health;
+            Debug.Log("Health Gained " + x + ", New Health: " + health);
+        }
+    }
+
+    //lose health
+    public void takeDamage(int x)
+    {
+        if(health - x <= 0)
+        {
+            //call death function
+            Debug.Log("Player Died");
+        }
+        else
+        {
+            health -= x;
+            healthSlider.value = health;
+            Debug.Log("Damage Taken: " + x + ", New Health: " + health);
+        }       
+    }
+        
+
+    //gain shield
+    public void gainShield(int x)
+    {
+        if(shield + x >= maxShield)
+        {
+            shield = maxShield;
+        }
+        else
+        {
+            shield += x;
+            Debug.Log("Shield Gained: " + x + ", New Shield: " + health);
+        }
+    }
+
+    //reduce shield
+    public void loseShield(int x)
+    {
+        if(shield - x < 0)
+        {
+            shield = 0;
+        }
+        else
+        {
+            shield -= x;
+            Debug.Log("Shield Lost: " + x + ", New Shield: " + health);
+        }
+    }
+
+    public void gainPowerGuage(int x)
+    {
+        if(powerGuage + x > maxPowerGuage)
+        {
+            powerGuage = maxPowerGuage;
+        }
+        else
+        {
+            powerGuage += x;
+            Debug.Log("Power Guage Gained: " + x + ", New Power Guage: " + health);
+        }
+    }
+
+    public void losePowerGuage(int x)
+    {
+        if(powerGuage - x < 0)
+        {
+            powerGuage = 0;
+        }
+        else
+        {
+            powerGuage -= x;
+            Debug.Log("Powe Guage Lost: " + x + ", New Power Guage: " + health);
+        }
+    }
+}
