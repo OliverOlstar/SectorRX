63a2ca9c58b181e495c5b6be785d8f25e786bf6c
ScottCWatman <48034730+ScottCWatman@users.noreply.github.com>
2019-08-19 10:30:33 -0400

    PauseMenu (WithBasicTransitions)
    Add Outer Shell UI and better transitions

---
+++ KajiuCollesuem/Assets/Scripts/MenuChange.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuChange : MonoBehaviour
+{
+    public void SetMenuOff()
+    {
+        gameObject.SetActive(false);
+    }
+
+    public void SetMenuOn()
+    {
+        gameObject.SetActive(true);
+    }
+}
