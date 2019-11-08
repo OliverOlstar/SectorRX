c1c3732a0590775df9f5971ced106457e5e123dd
Oliver <Olstar_123@users.noreply.github.com>
2019-11-08 18:50:30 -0500

    Added SavingLoading Script

---
+++ KajiuCollesuem/Assets/Scripts/Saving&Loading.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+    /*
+ Saves:
+
+ Powers Unlocked
+ Player Upgrades
+ Current Player Spawnpoint
+ Which Cells have already been collected
+
+
+ Loads:
+
+ Everything in save and
+ Respawns all enemies
+    */
+
+public class SavingLoading : MonoBehaviour
+{
+    public void Save(int pSlot = 0)
+    {
+
+
+        PlayerPrefs.Save();
+    }
+
+    public void LoadSave(int pSlot = 0)
+    {
+
+    }
+}
