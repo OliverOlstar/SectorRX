30ea4568e6e77c54c65c907638014e4f5c18d5f8
Oliver <Olstar_123@users.noreply.github.com>
2019-11-08 19:11:24 -0500

    Undid merging with scott's branch

---
+++ KajiuCollesuem/Assets/Scripts/SaveAndLoad.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+Oliver
+    Save:
+
+    Powers Player Has
+    Player Upgrades
+    Cells Collected
+    Current Checkpoint
+
+
+    Load:
+
+    Everything in save and
+    Respawn all enemies
+
+ */
+
+public class SaveAndLoad : MonoBehaviour
+{
+    public void Save(int pSlot = 0)
+    {
+
+
+        PlayerPrefs.Save();
+    }
+
+    public void LoadSave(int pSlot = 0)
+    {
+
+    }
+
+    public void RespawnEnemies()
+    {
+
+    }
+}
