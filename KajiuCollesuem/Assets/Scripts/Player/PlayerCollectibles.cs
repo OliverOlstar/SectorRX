1198e5444f41a80cc59a4413b00c851b6bf6060e
Scott Watman <scott.watman@rogers.com>
2020-01-06 11:25:36 -0500

    Tutorials

--- KajiuCollesuem/Assets/Scripts/Player/PlayerCollectibles.cs
+++ KajiuCollesuem/Assets/Scripts/Player/PlayerCollectibles.cs
@@ -6,16 +6,21 @@ using UnityEngine.UI;
 public class PlayerCollectibles : MonoBehaviour
 {
     HUDManager playerHUD;
+
     //public GameObject cell;
     public GameObject core;
+    public GameObject tutorialPrompt;
+    public bool firstCore;
 
     private void Start()
     {
         playerHUD = GameObject.FindGameObjectWithTag("HUD").GetComponent<HUDManager>();
+        firstCore = false;
     }
 
     private void OnTriggerEnter(Collider collision)
     {
+        //If player collides with Power Core item
         if (collision.gameObject.tag == "Core")
         {
             playerHUD.coreUIOn = true;
@@ -23,6 +28,13 @@ public class PlayerCollectibles : MonoBehaviour
             playerHUD.coreUI.SetActive(true);
             playerHUD.coreCounter = playerHUD.coreCounter + 1;
             playerHUD.SetCoreCount();
+            
+            //Show tutorial for upgrading after 
+            if(firstCore == false)
+            {
+                tutorialPrompt.SetActive(true);
+                firstCore = true;
+            }
         }
 
         //if (collision.gameObject.tag == "Cell")
