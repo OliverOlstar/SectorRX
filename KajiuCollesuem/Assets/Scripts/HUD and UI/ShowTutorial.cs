8a44ad9816b7149b0433cfe7a744ee1961b09974
Scott Watman <scott.watman@rogers.com>
2020-01-06 15:08:30 -0500

    Tutorial Update

--- KajiuCollesuem/Assets/Scripts/HUD and UI/ShowTutorial.cs
+++ KajiuCollesuem/Assets/Scripts/HUD and UI/ShowTutorial.cs
@@ -6,23 +6,41 @@ using UnityEngine.UI;
 public class ShowTutorial : MonoBehaviour
 {
     public GameObject tutorialUI = null;
+    public Text tutorialText;
+    PauseMenu pauseMenu;
 
     private void Start()
     {
         tutorialUI.SetActive(false);
+        pauseMenu = GameObject.FindGameObjectWithTag("HUD").GetComponent<PauseMenu>();
+    }
+
+    private void Update()
+    {
+        if(pauseMenu.pause)
+        {
+            tutorialUI.SetActive(false);
+            tutorialText.gameObject.SetActive(false);
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.tag == "Player")
+        if(other.tag == "DashTutorial")
         {
             tutorialUI.SetActive(true);
+            tutorialText.text = "Press Space or 'B' to Dash";
+        }
+        if(other.tag == "LockOnTutorial")
+        {
+            tutorialUI.SetActive(true);
+            tutorialText.text = "Right Click or Press the Right Stick to lock on to enemies";
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.tag == "Player")
+        if (other.tag == "DashTutorial")
         {
             tutorialUI.SetActive(false);
         }
