7744bde1b244c82eadf9d9e779794f0fac92d2d4
Chivarubhen <rmchiva925@gmail.com>
2020-01-13 20:44:02 -0500

    made more changes to combos

--- KajiuCollesuem/Assets/Scripts/Player/Rubhen scripts player/PlayerControllerR.cs
+++ KajiuCollesuem/Assets/Scripts/Player/Rubhen scripts player/PlayerControllerR.cs
@@ -5,20 +5,20 @@ using UnityEngine;
 public class PlayerControllerR : MonoBehaviour
 {
     //movement variables
-    private Vector3 moveVector = Vector3.zero;
-    private Vector3 lastMove;
-    public float speed = 8;
+    //private Vector3 moveVector = Vector3.zero;
+    //private Vector3 lastMove;
+    //public float speed = 8;
 
     //jumping variables
-    public float jumpForce = 8;
-    public float gravity = 25;
-    private float verticalVelocity;
-    private Vector3 normal;
-    private Vector3 reflection;
-    private Vector3 velocity;
+    //public float jumpForce = 8;
+    //public float gravity = 25;
+    //private float verticalVelocity;
+    //private Vector3 normal;
+    //private Vector3 reflection;
+    //private Vector3 velocity;
 
     //character controller for the player
-    public CharacterController myController;
+    //public CharacterController myController;
 
     //Combos related
     public Animator anim;
@@ -26,7 +26,7 @@ public class PlayerControllerR : MonoBehaviour
     float lastClickedTime = 0;
     public float maxComboDelay = 0.9f;
 
-    public AnimationState animState;
+    //public Transform modelPivot;
 
 
     // Start is called before the first frame update
@@ -40,33 +40,41 @@ public class PlayerControllerR : MonoBehaviour
     void Update()
     {
 
-        moveVector = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")); //movement based on axis
-        moveVector = transform.TransformDirection(moveVector); //transforming direction
-        moveVector = Vector3.ClampMagnitude(moveVector, 1f); //making the character move same speed in diagonals
+        //moveVector = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")); //movement based on axis
+        //moveVector = transform.TransformDirection(moveVector); //transforming direction
+        //transform.localRotation = Quaternion.Euler(moveVector);
+        //moveVector = Vector3.ClampMagnitude(moveVector, 1f); //making the character move same speed in diagonals
 
-        if (myController.isGrounded)
-        {
-            verticalVelocity = -1f; //constant force down y to keep object forced to ground during inclined planes
+        //if (myController.isGrounded)
+        //{
+        //    verticalVelocity = -1f; //constant force down y to keep object forced to ground during inclined planes
 
-            if (Input.GetButtonDown("Jump"))
-            {
-                verticalVelocity = jumpForce; //verticalVelocity is only affected by the jumpForce value
-            }
-        }
+        //    if (Input.GetButtonDown("Jump"))
+        //    {
+        //        verticalVelocity = jumpForce; //verticalVelocity is only affected by the jumpForce value
+        //    }
+        //}
 
-        else
-        {
-            verticalVelocity -= gravity * Time.deltaTime; //at all other times gravity is constantly being applied to the y
-            moveVector = lastMove; //setting new vector value every frame if no jump
-        }
+        //else
+        //{
+        //    verticalVelocity -= gravity * Time.deltaTime; //at all other times gravity is constantly being applied to the y
+        //    moveVector = lastMove; //setting new vector value every frame if no jump
+        //}
 
-        moveVector.y = 0; //setting the y to zero
-        moveVector.Normalize(); //normalizing the vector as a whole but only caring about x and z
-        moveVector *= speed; //speed is applied to this vector to move it
-        moveVector.y = verticalVelocity; //vertical velocity affect y but is dependent on the jump
+        //moveVector.y = 0; //setting the y to zero
+        //moveVector.Normalize(); //normalizing the vector as a whole but only caring about x and z
+        //moveVector *= speed; //speed is applied to this vector to move it
+        //moveVector.y = verticalVelocity; //vertical velocity affect y but is dependent on the jump
 
-        myController.Move(moveVector * Time.deltaTime); //using the move function on character controller
-        lastMove = moveVector;
+        //if (numberOfClicks > 0)
+        //{
+        //    moveVector = Vector3.zero;
+        //}
+
+        //myController.Move(moveVector * Time.deltaTime); //using the move function on character controller
+        
+        
+        //lastMove = moveVector;
 
         if (Time.time - lastClickedTime > maxComboDelay)
         {
@@ -171,20 +179,20 @@ public class PlayerControllerR : MonoBehaviour
         numberOfClicks = 0;
     }
 
-    private void OnControllerColliderHit(ControllerColliderHit hit)
-    {
-        normal = hit.normal; //setting the hit normal as a variable
-        Vector3 reflection = Vector3.Reflect(moveVector, normal);
+    //private void OnControllerColliderHit(ControllerColliderHit hit)
+    //{
+    //    normal = hit.normal; //setting the hit normal as a variable
+    //    Vector3 reflection = Vector3.Reflect(moveVector, normal);
 
-        if (!myController.isGrounded && normal.y < 0.1f)
-        {
-            if (Input.GetButtonDown("Jump"))
-            {
-                verticalVelocity = jumpForce * 0.75f; //75% of original ground jump so not too powerful off the wall
-                moveVector = reflection * 3 * speed; //jumping on the direction of the normal to achieve wall jump
-            }
-        }
+    //    if (!myController.isGrounded && normal.y < 0.1f)
+    //    {
+    //        if (Input.GetButtonDown("Jump"))
+    //        {
+    //            verticalVelocity = jumpForce * 0.75f; //75% of original ground jump so not too powerful off the wall
+    //            moveVector = reflection * 3 * speed; //jumping on the direction of the normal to achieve wall jump
+    //        }
+    //    }
 
-    }
+    //}
 }
 
