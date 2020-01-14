871a1754a782888c9b0745d954658395359a8c1b
Scott Watman <scott.watman@rogers.com>
2020-01-14 10:23:07 -0500

    GroundPoundStart

---
+++ KajiuCollesuem/Assets/Scripts/Player/Player Powers/GroundPound.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundPound : MonoBehaviour
+{
+    PlayerMovement _PMovement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _PMovement = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMovement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
