7d953e03729fc183babd169a65e5e0c8f6ce6cca
Scott Watman <scott.watman@rogers.com>
2020-01-17 17:14:34 -0500

    Multiplayer and Splitscreen

---
+++ KajiuCollesuem/Assets/Code/HUD and UI/Scripts/connectedPlayers.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/*Programmer: Scott Watman
+ Additional Programmer(s): Oliver Loescher
+ Description: Allows multiple players to connect and play the game*/
+
+public class connectedPlayers : MonoBehaviour
+{
+    public static int playersConnected = 0;
+    public Text playerCount;
+
+    public  void OnPlayerJoined()
+    {
+        playersConnected++;
+        Debug.Log("OnPlayerJoined " + playersConnected);
+        playerCount.text = "Number of Players: " + playersConnected.ToString();
+    }
+}
