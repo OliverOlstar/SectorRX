0277424d5f4277d2842c51eb87efd8ad750fb867
Scott Watman <scott.watman@rogers.com>
2020-01-27 12:26:02 -0500

    Splitscreen Toggle & Collect Cells

---
+++ KajiuCollesuem/Assets/Code/HUD and UI/Scripts/MenuInput/ChooseFighter.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ChooseFighter : MonoBehaviour
+{
+    public List<CharacterSelect> characters = new List<CharacterSelect>();
+    public GameObject charCellPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach(CharacterSelect character in characters)
+        {
+            CreateCharacterCell(character);
+        }
+    }
+
+    private void CreateCharacterCell(CharacterSelect character)
+    {
+        GameObject charCell = Instantiate(charCellPrefab, transform);
+
+        Image artwork = charCell.transform.Find("Artwork").GetComponent<Image>();
+        Text charName = charCell.transform.Find("CharacterName").GetComponentInChildren<Text>();
+
+        artwork.sprite = character.characterSprite;
+        charName.text = character.characterName;
+    }
+}
