cac99a8c3fe970d5b1b4cb8249c72fd46e637701
Scott Watman <scott.watman@rogers.com>
2020-01-28 11:02:07 -0500

    Player Joined UI

---
+++ KajiuCollesuem/Assets/Code/Systems/AssignedPlayer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class AssignedPlayer : MonoBehaviour
+{
+    public PlayerInput player;
+    public bool hasInput;
+
+    private void Start()
+    {
+        hasInput = true;
+    }
+}
