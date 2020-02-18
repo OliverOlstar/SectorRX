7d4cb940ab1eab4dd8d990175b15cdb35e5c3472
Scott Watman <scott.watman@rogers.com>
2020-02-18 11:15:43 -0500

    Update SplitscreenManager.cs

--- KajiuCollesuem/Assets/Code/Camera/Scripts/SplitscreenManager.cs
+++ KajiuCollesuem/Assets/Code/Camera/Scripts/SplitscreenManager.cs
@@ -9,8 +9,8 @@ using UnityEngine.UI;
 public class SplitscreenManager : MonoBehaviour
 {
     public List<Camera> playerCams = new List<Camera>();
-
-    public bool isHorizontalSplit;
+    private float verticalSplit = 1;
+    private float horizontalSplit = 1;
 
     public void SetSplitScreen(MatchManager pManager)
     {
@@ -20,12 +20,37 @@ public class SplitscreenManager : MonoBehaviour
             playerCams.Add(getCam);
         }
 
-        //Player 1 Camera settings when Horizontal.
-        playerCams[0].rect = new Rect(0.5f, 0.0f, 0.5f, 1.0f);
-        playerCams[0].fieldOfView = 55;
+        //Track amount of player cameras in game do determine horizontal and vertical split
+        if(playerCams.Count >= 7)
+        {
+            horizontalSplit = 3;
+            verticalSplit = 3;
+        }
+        else if(playerCams.Count >= 5)
+        {
+            horizontalSplit = 3;
+            verticalSplit = 2;
+        }
+        else if(playerCams.Count >= 3)
+        {
+            horizontalSplit = 2;
+            verticalSplit = 2;
+        }
+        else if(playerCams.Count == 2)
+        {
+            playerCams[0].rect = new Rect(0.0f, 0.0f, 0.5f, 1.0f);
+            playerCams[1].rect = new Rect(0.5f, 0.0f, 0.5f, 1.0f);
+            return;
+        }
 
-        //Player 2 Camera settings when Horizontal.
-        playerCams[1].rect = new Rect(0.0f, 0.0f, 0.5f, 1.0f);
-        playerCams[1].fieldOfView = 55;
+        float hor = 1.0f / horizontalSplit;
+        float ver = 1.0f / verticalSplit;
+
+        //Set vertical and horizontal values for all connected cameras
+        for (int i = 0; i < playerCams.Count; i++)
+        {
+            playerCams[i].rect = new Rect(hor * (i % horizontalSplit), (ver * (verticalSplit - 1)) - ver * Mathf.Floor(i / verticalSplit), hor, ver);
+            Debug.Log("Ver: " + Mathf.Floor(i / verticalSplit));
+        }
     }
 }
