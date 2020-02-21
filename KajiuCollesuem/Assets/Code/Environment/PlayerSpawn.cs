cc9c44f03a98b70c1934542dc7b19c804e444a25
Scott Watman <scott.watman@rogers.com>
2020-02-21 10:43:54 -0500

    Code

--- KajiuCollesuem/Assets/Code/Environment/PlayerSpawn.cs
+++ KajiuCollesuem/Assets/Code/Environment/PlayerSpawn.cs
@@ -144,7 +144,7 @@ public class PlayerSpawn : MonoBehaviour
             _SpawnPointIndex = Random.Range(0, (connectedPlayers.playersToSpawn <= 4 ? 4 : 9) - i);
             Transform _EightSpawnPos = playerSpawns[_SpawnPointIndex];
             playerSpawns.RemoveAt(_SpawnPointIndex);
-            GameObject playerCharacter = Instantiate(playerPrefab, _EightSpawnPos.position, _EightSpawnPos.rotation);
+            GameObject playerCharacter = Instantiate(playerPrefab, _EightSpawnPos.position, transform.rotation);
             //playerCharacter.transform.LookAt(Vector3.zero);
             //playerCharacter.transform.rotation = playerCharacter.transform.rotation * Quaternion.Euler(0, 90, 0);
             players.Add(playerCharacter);
