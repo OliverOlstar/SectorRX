51485a28e02fc9918696a2b6fc377ffb9b9ab156
Scott Watman <scott.watman@rogers.com>
2020-02-24 11:49:10 -0500

    Gamepad or Key Press Starts Game

--- KajiuCollesuem/Assets/Code/Inputs/DeviceHandler.cs
+++ KajiuCollesuem/Assets/Code/Inputs/DeviceHandler.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DeviceHandler : MonoBehaviour
 {
     [SerializeField] public connectedPlayers _AddPlayer;
     [SerializeField] private Panels playerPanel;
+    private int stateValue = 0;
 
     //Disconnects the player device from assigned slot if player has left and panel was assigned
     public void OnLeaving()
@@ -21,14 +23,28 @@ public class DeviceHandler : MonoBehaviour
     //Connects the player device to an open slot if player a panel has not been assigned to
     public void OnJoining()
     {
-        if (playerPanel == null)
+        switch (stateValue)
         {
-            playerPanel = _AddPlayer.OnDeviceJoined();
-            
-            if (playerPanel != null)
-            {
-                playerPanel.PlayerJoined();
-            }
+            case 0:
+                if (playerPanel == null)
+                {
+                    playerPanel = _AddPlayer.OnDeviceJoined();
+
+                    if (playerPanel != null)
+                    {
+                        playerPanel.PlayerJoined();
+                        stateValue = 1;
+                    }
+                }
+                break;
+
+            case 2:
+                if (_AddPlayer.hasJoined && _AddPlayer._Devices.Length >= 2)
+                {
+                    _AddPlayer.SetPlayerOrder();
+                    SceneManager.LoadScene(1);
+                }
+                break;
         }
     }
 
