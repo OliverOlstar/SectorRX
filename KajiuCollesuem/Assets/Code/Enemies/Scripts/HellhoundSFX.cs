ac91857ff611f4b9977c14a72c51ad802b912bc8
Scott Watman <scott.watman@rogers.com>
2020-02-25 11:23:00 -0500

    Hellhound Death SFX

---
+++ KajiuCollesuem/Assets/Code/Enemies/Scripts/HellhoundSFX.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class HellhoundSFX : MonoBehaviour
+{
+    public AudioClip[] deathSound = new AudioClip[2];
+    public AudioClip biteAttack;
+    public AudioSource sfxSource;
+
+    public void AEDeathSound()
+    {
+        sfxSource.clip = deathSound[Random.Range(0, 1)];
+        sfxSource.Play();
+    }
+}
