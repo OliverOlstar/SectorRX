24044c2216416bbb1b4ca6347e62d7d5fdafd392
Scott Watman <scott.watman@rogers.com>
2020-02-25 12:24:42 -0500

    Hellhound Shot SFX

--- KajiuCollesuem/Assets/Code/Enemies/Scripts/HellhoundSFX.cs
+++ KajiuCollesuem/Assets/Code/Enemies/Scripts/HellhoundSFX.cs
@@ -7,7 +7,14 @@ public class HellhoundSFX : MonoBehaviour
 {
     public AudioClip[] deathSound = new AudioClip[2];
     public AudioClip biteAttack;
+    public AudioClip shotAttack;
     public AudioSource sfxSource;
+
+    public void AEShotSound()
+    {
+        sfxSource.clip = shotAttack;
+        sfxSource.Play();
+    }
 
     public void AEAttackSound()
     {
