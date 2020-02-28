96cb50b0cdb409c721613480ff15cf331c53dcab
Scott Watman <scott.watman@rogers.com>
2020-02-28 12:33:11 -0500

    Enemy SFX

---
+++ KajiuCollesuem/Assets/Code/Enemies/Scripts/EnemySFX.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class EnemySFX : MonoBehaviour
+{
+    public AudioSource enemySource;
+    public AudioClip[] enemySFX = new AudioClip[4];
+
+    public void BiteSound()
+    {
+
+    }
+
+    public void ShotSound()
+    {
+
+    }
+
+    public void DeathSound()
+    {
+
+    }
+}
