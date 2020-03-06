217d62adfc1301d77164ec8e7957ed59e76b2c92
Mugiesshan Anandarajah <manandarajah@users.noreply.github.com>
2020-03-06 10:52:50 -0500

    Worked on PlasmaBall a bit. Still needs work to do
    I worked on the PlasmaBall. I got the prefab and starter code. So far it's functional but not fully functional.

---
+++ KajiuCollesuem/Assets/Scenes/AbilityTest/PlasmaBall.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlasmaBall : MonoBehaviour
+{
+    [SerializeField] private Transform _pbSpawnPoint;
+    [SerializeField] private Rigidbody _pbPrefab;
+    [SerializeField] private float _pbSpeed, _pbScaleIncrementRate;
+    private Rigidbody _plasmaBallInstance = null;
+    private bool _releasePlasmaBall = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!_releasePlasmaBall && Input.GetKey(KeyCode.V))
+        {
+           if (!_plasmaBallInstance) {
+                _plasmaBallInstance = Instantiate(_pbPrefab) as Rigidbody;
+                _plasmaBallInstance.transform.position = _pbSpawnPoint.position;
+           }
+
+            else
+                _plasmaBallInstance.transform.localScale += Vector3.one * _pbScaleIncrementRate;
+
+            _releasePlasmaBall = Input.GetKeyUp(KeyCode.V);
+        }
+
+        else if (_plasmaBallInstance)
+        {
+            _plasmaBallInstance.isKinematic = false;
+            _plasmaBallInstance.AddForce((transform.forward * _pbSpeed) + Vector3.up, ForceMode.Impulse);
+            _plasmaBallInstance = null;
+            _releasePlasmaBall = false;
+        }
+    }
+}
---
+++ KajiuCollesuem/Assets/Scenes/AbilityTest/PlasmaBallHitbox.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlasmaBallHitbox : MonoBehaviour
+{
+    [SerializeField] private float _halfPlayerHeight;
+    [SerializeField] private GameObject _plasmaBallField;
+
+    /* I was thinking we could have a class variable for damage, and if the plasma ball hits the enemy directly, we multiply the
+     * damage variable for twice the damage, and pass that into the TakeDamage method. If not we just pass in the damage value as
+     * it is.
+     * */
+    public static float damage;
+    private Rigidbody _rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (IsOnGround())
+        {
+            _plasmaBallField.SetActive(true);
+            Debug.Log("Burn everything within collision sphere");
+        }
+    }
+
+    private bool IsOnGround()
+    {
+        // Linecast get two points
+        Vector3 lineStart = transform.position;
+        Vector3 vectorToSearch = new Vector3(lineStart.x, lineStart.y - _halfPlayerHeight, lineStart.z);
+
+        // Debug Line
+        Color color = new Color(0.0f, 0.0f, 1.0f);
+        Debug.DrawLine(lineStart, vectorToSearch, color);
+
+        // Linecast
+        RaycastHit hitInfo;
+        if (Physics.Linecast(this.transform.position, vectorToSearch, out hitInfo))
+        {
+            // On Ground
+            return true;
+        }
+
+        // Off Ground
+        return false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag.Equals("Enemy"))
+            Destroy(this.gameObject);
+    }
+}
