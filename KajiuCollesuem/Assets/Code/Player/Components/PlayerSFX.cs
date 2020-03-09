5b7df2d598dba735fd80fc1ddc31f237b3835b0d
Scott Watman <scott.watman@rogers.com>
2020-03-09 10:43:19 -0400

    Plasma Strike

--- KajiuCollesuem/Assets/Code/Player/Components/PlayerSFX.cs
+++ KajiuCollesuem/Assets/Code/Player/Components/PlayerSFX.cs
@@ -106,7 +106,7 @@ public class PlayerSFX : MonoBehaviour
     //Plays sound when playeer uses Plasma Strike ability.
     public void PlasmaStrikeSound()
     {
-        PlaySound(0.0f, 0.4f, 1.0f, abilitySounds[3], sfxSource);
+        PlaySound(0.0f, 0.4f, 1.0f, abilitySounds[1], sfxSource);
     }
     #endregion
 
