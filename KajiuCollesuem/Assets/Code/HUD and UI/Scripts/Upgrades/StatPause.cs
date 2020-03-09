228323b1cc71a8cf7833a5961532a4a61cf9b02a
Scott Watman <scott.watman@rogers.com>
2020-03-09 16:12:21 -0400

    Stat Menus

---
+++ KajiuCollesuem/Assets/Code/HUD and UI/Scripts/Upgrades/StatPause.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatPause : MonoBehaviour
+{
+    public GameObject[] statsMenu = new GameObject[7];
+
+    private void Start()
+    {
+        foreach (GameObject stats in statsMenu)
+        {
+            stats.SetActive(false);
+        }
+    }
+
+    public void ShowStatsOn()
+    {
+        foreach(GameObject stats in statsMenu)
+        {
+            stats.SetActive(true);
+        }
+    }
+
+    public void ShowStatsOff()
+    {
+        foreach (GameObject stats in statsMenu)
+        {
+            stats.SetActive(false);
+        }
+    }
+}
