edd3277bdb5f39aec27f400157bc819c1ed762b8
Oliver Loescher <48457872+Olstar72@users.noreply.github.com>
2020-03-10 17:26:24 -0400

    Fixed Infinite hit bug

--- KajiuCollesuem/Assets/Code/Player/PlayerMultHitbox.cs
+++ KajiuCollesuem/Assets/Code/Player/PlayerMultHitbox.cs
@@ -35,10 +35,13 @@ public class PlayerMultHitbox : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        //Check if collided with an Attributes Script
+        // Check if collided with an Attributes Script
         IAttributes otherAttributes = other.GetComponent<IAttributes>();
         if (otherAttributes == null) 
             otherAttributes = other.GetComponentInParent<IAttributes>();
+
+        // Don't damage player abilities
+        if (other.CompareTag("Fireball")) return;
 
         if (otherAttributes != null && otherAttributes.IsDead() == false && otherAttributes != _playerIAttributes && !collidersInTrigger.Contains(other))
         {
@@ -67,7 +70,7 @@ public class PlayerMultHitbox : MonoBehaviour
             yield return new WaitForSeconds(_damageRate);
         }
         // Check if still colliding
-        while (collidersInTrigger.Contains(pOther) && pOther.gameObject.activeSelf && pAttributes.IsDead() == false);
+        while (collidersInTrigger.Contains(pOther) && pAttributes.IsDead() == false);
 
         // If in Tar still but is dead
         if (collidersInTrigger.Contains(pOther))
