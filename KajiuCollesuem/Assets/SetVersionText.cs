06c8feb9fa6effc637725870866c0bde85da0e1d
Oliver Loescher <48457872+Olstar72@users.noreply.github.com>
2020-03-13 11:08:36 -0400

    Updated Verison number

--- KajiuCollesuem/Assets/SetVersionText.cs
+++ KajiuCollesuem/Assets/SetVersionText.cs
@@ -7,6 +7,6 @@ public class SetVersionText : MonoBehaviour
 {
     private void Awake()
     {
-        GetComponent<Text>().text = "Pre-Alpha 0.6.4";
+        GetComponent<Text>().text = "Pre-Alpha 0.8.9";
     }
 }
