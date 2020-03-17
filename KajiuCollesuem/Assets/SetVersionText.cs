7a621f809f18980d68549ff31061f298fdd60f7e
Oliver <Olstar_123@users.noreply.github.com>
2020-03-17 16:54:56 -0400

    Added John's final particle

--- KajiuCollesuem/Assets/SetVersionText.cs
+++ KajiuCollesuem/Assets/SetVersionText.cs
@@ -7,6 +7,6 @@ public class SetVersionText : MonoBehaviour
 {
     private void Awake()
     {
-        GetComponent<Text>().text = "Pre-Alpha 0.9.2";
+        GetComponent<Text>().text = " ";
     }
 }
